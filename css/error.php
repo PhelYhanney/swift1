88ff35122c88e5093572c7a84c55957255e66130
WorkAccount20 <gitmail236@gmail.com>
2019-12-10 18:36:14 +0000

    work

---
+++ css/error.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error!</title>
+</head>
+<body>
+		<center><h1>Page Unavaialble....<a href="index.php">click</a> here to redirect</h1></center>
+</body>
+</html>
